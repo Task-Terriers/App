358cf6eee25814ce8b6d7b701f398dce7d8f9052
Youngjin <yjshin229@gmail.com>
2023-11-29 08:05:45 -0500

    add flatlist

--- TaskTerriers/src/Screens/ServicesTab.tsx
+++ TaskTerriers/src/Screens/ServicesTab.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { SafeAreaView, View, StyleSheet, Text, TouchableOpacity } from 'react-native'
+import { SafeAreaView, View, StyleSheet, Text, TouchableOpacity, FlatList } from 'react-native'
 import TaskTerriersSafeAreaView from '../Views/TaskTerriersSafeAreaView'
 import { Col } from '../StyleToProps/Col'
 import { Span } from '../StyleToProps'
@@ -9,8 +9,10 @@ import { UniversalButton } from '../components/Buttons'
 import { TaskTerriersNavigationModule } from '../modules/NavigationModule'
 import { Auth, Root } from '../navigation/type'
 import AsyncStorageModule from '../modules/AsyncStorageModule'
+import { SerivcesCard } from '../components/Card'
+import { BottomTabBarHeightContext } from '@react-navigation/bottom-tabs'
 
-interface Props {}
+interface Props { }
 
 const ServicesTab = ({ route }) => {
   /*********
@@ -24,6 +26,120 @@ const ServicesTab = ({ route }) => {
   /*************
    * state, ref
    *************/
+
+  const mockRequestsCardData = [
+    {
+      firstName: "John",
+      lastName: "Doe",
+      postPreview: "Exploring Microbiology",
+      hideKebabMenu: false,
+      profilePicPath: "https://picsum.photos/seed/696/3000/2000",
+      major: "Biology",
+      numOfReview: 12,
+      reviewRate: 4.7,
+      serviceRate: 150,
+      onPress: () => onPressCard()
+    },
+    {
+      firstName: "Jane",
+      lastName: "Smith",
+      postPreview: "Art in the Renaissance",
+      hideKebabMenu: true,
+      profilePicPath: "https://picsum.photos/200/300?grayscale",
+      major: "Art History",
+      numOfReview: 8,
+      reviewRate: 4.3,
+      serviceRate: 120,
+      onPress: () => onPressCard
+    },
+    {
+      firstName: "Alice",
+      lastName: "Johnson",
+      postPreview: "The Basics of Quantum Physics",
+      hideKebabMenu: false,
+      profilePicPath: "https://picsum.photos/200/300/?blur",
+      major: "Physics",
+      numOfReview: 15,
+      reviewRate: 4.9,
+      serviceRate: 200
+    },
+    {
+      firstName: "Bob",
+      lastName: "Brown",
+      postPreview: "Modern Architecture and Design",
+      hideKebabMenu: true,
+      profilePicPath: "https://picsum.photos/seed/706/3000/2000",
+      major: "Architecture",
+      numOfReview: 5,
+      reviewRate: 4.0,
+      serviceRate: 95
+    },
+    {
+      firstName: "Carol",
+      lastName: "Davis",
+      postPreview: "Exploring World History",
+      hideKebabMenu: false,
+      profilePicPath: "https://picsum.photos/seed/106/3000/2000",
+      major: "History",
+      numOfReview: 20,
+      reviewRate: 4.8,
+      serviceRate: 180
+    },
+    {
+      firstName: "David",
+      lastName: "Miller",
+      postPreview: "Introduction to Computer Science",
+      hideKebabMenu: true,
+      profilePicPath: "https://picsum.photos/seed/640/3000/2000",
+      major: "Computer Science",
+      numOfReview: 10,
+      reviewRate: 4.2,
+      serviceRate: 110
+    },
+    {
+      firstName: "Eva",
+      lastName: "Wilson",
+      postPreview: "Theories of Economics",
+      hideKebabMenu: false,
+      profilePicPath: "https://picsum.photos/seed/576/3000/2000",
+      major: "Economics",
+      numOfReview: 7,
+      reviewRate: 4.1,
+      serviceRate: 100
+    },
+    {
+      firstName: "Frank",
+      lastName: "Anderson",
+      postPreview: "Understanding Chemical Reactions",
+      hideKebabMenu: true,
+      major: "Chemistry",
+      numOfReview: 13,
+      reviewRate: 4.6,
+      serviceRate: 140
+    },
+    {
+      firstName: "Grace",
+      lastName: "Thomas",
+      postPreview: "Advanced Mathematics for Engineers",
+      hideKebabMenu: false,
+      major: "Engineering",
+      numOfReview: 9,
+      reviewRate: 4.4,
+      serviceRate: 130
+    },
+    {
+      firstName: "Henry",
+      lastName: "Jackson",
+      postPreview: "Political Science and Modern Governance",
+      hideKebabMenu: true,
+      profilePicPath: "https://picsum.photos/seed/400/3000/2000",
+      major: "Political Science",
+      numOfReview: 11,
+      reviewRate: 4.5,
+      serviceRate: 125
+    }
+  ];
+
 
   const [isRendering, setIsRendering] = useState<boolean>(true)
 
@@ -44,7 +160,7 @@ const ServicesTab = ({ route }) => {
    * functions
    ************/
 
-  const onPressButton = () => {
+  const onPressCard = () => {
     return TaskTerriersNavigationModule.navigate(Root.ServiceDetailScreen)
   }
   const onPressToAddProfile = async () => {
@@ -61,7 +177,7 @@ const ServicesTab = ({ route }) => {
   }
 
   const renderButton = () => {
-    return <UniversalButton size="medium" text={{ value: 'Go to Detail Screen' }} onPress={onPressButton} />
+    return <UniversalButton size="medium" text={{ value: 'Go to Detail Screen' }} onPress={onPressCard} />
   }
 
   /***********
@@ -71,12 +187,21 @@ const ServicesTab = ({ route }) => {
   return (
     <TaskTerriersSafeAreaView style={{ flex: 1 }}>
       {renderNavigationBar()}
-      <Col p16>
+      <Col p16 mb100>
         {renderButton()}
-        <UniversalButton size="medium" text={{ value: 'Go to Add profile Screen' }} onPress={onPressToAddProfile} />
+        <Col mb20>
+        </Col>
+        <FlatList
+          data={mockRequestsCardData}
+          renderItem={({ item }) => <SerivcesCard {...item} />}
+          keyExtractor={(item, index) => index.toString()}
+          contentContainerStyle={{}}
+        />
       </Col>
     </TaskTerriersSafeAreaView>
   )
 }
 
 export default ServicesTab
+
+
--- TaskTerriers/src/components/Card/SerivcesCard.tsx
+++ TaskTerriers/src/components/Card/SerivcesCard.tsx
@@ -1,22 +1,23 @@
 import React, { useState, useEffect } from 'react'
-import { SafeAreaView, View, StyleSheet, Text, TouchableOpacity } from 'react-native'
-import { Row, Span } from '../../StyleToProps'
+import { Col, Row, Span } from '../../StyleToProps'
+import { Image } from 'expo-image'
 
 interface RequestsCardProps {
   // datasource: {} this should come from the backend.
   // will use different props for now to show example.
   firstName: string
   lastName: string
-  date: Date
-  numberOfComments?: number
   postPreview?: string
   hideKebabMenu?: boolean
+  profilePicPath?: string
+  major: string
+  numOfReview: number,
+  reviewRate: number,
+  serviceRate: number
+  onPress?: () => void
 }
 
-const SerivcesCard: React.FC<RequestsCardProps> = ({ firstName, lastName, date, numberOfComments, postPreview, hideKebabMenu }) => {
-  /*********
-   * recoil
-   *********/
+const SerivcesCard: React.FC<RequestsCardProps> = ({ firstName, lastName, postPreview, hideKebabMenu, profilePicPath, major, numOfReview, reviewRate, serviceRate, onPress }) => {
 
   /**************************
    * props, navigation prams
@@ -27,6 +28,8 @@ const SerivcesCard: React.FC<RequestsCardProps> = ({ firstName, lastName, date, 
    *************/
 
   const [isRendering, setIsRendering] = useState<boolean>(true)
+  const blurhash =
+    '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj['
 
   /**************
    * life cycles
@@ -48,30 +51,66 @@ const SerivcesCard: React.FC<RequestsCardProps> = ({ firstName, lastName, date, 
   /*********
    * render
    *********/
+  const renderProfilePic = () => {
+    return (
+      <Col alignCenter mr16 radius100 overflow={'hidden'}>
+        {!profilePicPath ? <Image contentFit="fill" source={require('../../assets/images/defaultProfile.jpeg')} style={{ width: 56, height: 56 }} /> :
+          <Image contentFit="fill" source={profilePicPath} style={{ width: 56, height: 56 }} />}
+      </Col>
+    )
+  }
+
+  const renderMajor = () => {
+    return (
+      <Span titleS>
+        Major : {major}
+      </Span>
+    )
+  }
+
+  const renderDescriptionPreview = () => {
+    return (
+      <Span bodyM colorNeutral40>{postPreview}</Span>
+    )
+  }
+
+  const renderServiceRate = () => {
+    return (
+      <Span headlineL colorBURed>$ {serviceRate}</Span>
+    )
+  }
 
   const renderName = () => {
-    ;<Row>
-      <Span labelL>
+    return (
+      <Span labelL mb6>
         {firstName} • {lastName}
       </Span>
-    </Row>
+    )
   }
 
-  const renderDate = () => {
-    ;<Row>
-      <Span labelM colorNeutral70>
-        {date.getDate()}
-      </Span>
-    </Row>
-  }
 
   /***********
    * render()
    ***********/
 
-  return <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}></SafeAreaView>
+  return (
+    <Col bgNeutral100 h150 radius12 ph12 pv14 mb12 onPress={onPress}>
+      <Row mb12 justifyBetween>
+        <Row>
+          {renderProfilePic()}
+          <Col>
+            {renderName()}
+            {renderMajor()}
+          </Col>
+        </Row>
+        {renderServiceRate()}
+      </Row>
+      <Col>
+        {renderDescriptionPreview()}
+      </Col>
+    </Col>
+  )
 }
 
-const styles = StyleSheet.create({})
 
 export { SerivcesCard }
